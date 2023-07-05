061814a230373b572b08066e138b9b8cf04268f3
Fred Meziere <Fredzem@outlook.fr>
2023-07-05 19:24:29 +0200

    Feat/refacto : Modification des pages About/Contact et Error 404

--- src/pages/Error404/error404.js
+++ src/pages/Error404/error404.js
@@ -2,13 +2,13 @@ import './styles.scss';
 
 function Error404() {
   return (
-    <div className="errorPage">
-      <div style={{ textAlign: 'center' }}>
-        <h1>Erreur 404: Page non trouvée</h1>
-        <p>Désolé, la page que vous recherchez est introuvable.</p>
+    <div className="errorpage">
+      <div className="errorpage-div">
+        <h1 className="errorpage-h1">Erreur 404: Page non trouvée</h1>
+        <p className="errorpage-p">Désolé, mais malheuresement la page que vous recherchez est introuvable.</p>
       </div>
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
-        <img className="errorpage-img" src={`${process.env.PUBLIC_URL}/femme yoga error 404.jpg`} alt="Erreur 404" width="700" />
+      <div className="errorpage-div">
+        <img className="errorpage-img" src={`${process.env.PUBLIC_URL}/femme_yoga_error_404.jpg`} alt="Erreur 404" width="700" />
       </div>
     </div>
   );
