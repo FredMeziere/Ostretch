fba3d42ae21890fd75f38ffcfc4c917ab5db46d6
Fred Meziere <Fredzem@outlook.fr>
2023-07-05 21:35:44 +0200

    Feat: Modification du composant infoForm (propstypes validation/selecteur css suppression de variables non utilisées 'response')

--- src/components/InfoForm/index.js
+++ src/components/InfoForm/index.js
@@ -1,106 +1,111 @@
-import { useState } from "react";
+import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { object, func } from 'prop-types';
 
-const InfoForm = ({user, setUser, setIsEditOpen, setIsLogged}) => {
+function InfoForm({
 
+  user, setUser, setIsEditOpen, setIsLogged,
+}) {
   const navigate = useNavigate();
   const token = localStorage.getItem('token');
 
   const [userValue, setUserValue] = useState({
-    username: "",
-    biography: ""
-  })
+    username: '',
+    biography: '',
+  });
 
-    const handleChange = (event) => {
-      const { name, value } = event.target;
-      setUserValue({
-          ...userValue,
-          [name]: value
-        })
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setUserValue({
+      ...userValue,
+      [name]: value,
+    });
+  };
+  //
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const updatedData = {};
+
+    if (userValue.username !== '') {
+      updatedData.username = userValue.username;
     }
-    //
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
+    if (userValue.biography !== '') {
+      updatedData.biography = userValue.biography;
+    }
 
-
-      const updatedData = {};
-
-      if (userValue.username !== "") {
-        updatedData.username = userValue.username;
-      }
-    
-      if (userValue.biography !== "") {
-        updatedData.biography = userValue.biography;
-      }
-
-      const token = localStorage.getItem('token'); // Récupérer le jeton d'authentification stocké dans le stockage local
-      console.log(token)
-
-      axios.patch(`${process.env.REACT_APP_BASE_URL}/user/me`, updatedData, {
+    axios.patch(`${process.env.REACT_APP_BASE_URL}/user/me`, updatedData, {
       headers: {
-        'Authorization': `Bearer ${token}`, // Ajouter le jeton d'authentification à l'en-tête de la demande
-        "Content-Type": "application/json"
-      }
+        Authorization: `Bearer ${token}`, // Ajouter le jeton d'authentification à l'en-tête de la demande
+        'Content-Type': 'application/json',
+      },
     })
-      .then(response => {
+      .then(() => {
         axios.get(`${process.env.REACT_APP_BASE_URL}/user/me`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         })
-        .then((response) => {
-          const userFound = response.data.filtredUserInfo;
-          setUser(userFound);
-          console.log(user);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+          .then((response) => {
+            const userFound = response.data.filtredUserInfo;
+            setUser(userFound);
+          })
+          .catch((error) => {
+            toast.error(error);
+          });
       })
-      .catch(error => {
-        console.error(error);
+      .catch((error) => {
+        toast.error(error);
       });
-      setIsEditOpen(false);
-    }
+    setIsEditOpen(false);
+  };
 
-    const handleDelete = () => {
-      const config = {
-        headers: { Authorization: `Bearer ${token}` }
-      };
-      axios.delete(`${process.env.REACT_APP_BASE_URL}/user/me`, config)
-      .then(response => {
-        setUser("");
+  const handleDelete = () => {
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    axios.delete(`${process.env.REACT_APP_BASE_URL}/user/me`, config)
+      .then(() => {
+        setUser('');
         setIsLogged(false);
         localStorage.clear();
-        navigate("/signup");
+        navigate('/signup');
       })
-      .catch(error => {
-        console.log(error);
+      .catch((error) => {
+        toast.error(error);
       });
-    }
+  };
 
-    return (
-       
+  return (
+
     <form className="infos-container" onSubmit={handleSubmit}>
-     <p>
-          Mon pseudo:
+      <p>
+        Mon pseudo:
       </p>
-        <input type="text" name="username" value={userValue.username} className='infos' placeholder={user.username} onChange={handleChange}/> 
-      
-        <p>
-          Ma bio:
-        </p>
-        <textarea rows="5" cols="33" type="text" name="biography" value={userValue.biography} className='infos'  placeholder={user.biography} onChange={handleChange}/>
-  
+      <input type="text" name="username" value={userValue.username} className="infos" placeholder={user.username} onChange={handleChange} />
 
-        <button className="modify-btn" >Enregistrer</button>
+      <p>
+        Ma bio:
+      </p>
+      <textarea rows="5" cols="33" type="text" name="biography" value={userValue.biography} className="infos" placeholder={user.biography} onChange={handleChange} />
+
+      <button className="modify-btn" type="button">Enregistrer</button>
       <p>Vous souhaitez supprimer définivement votre compte?</p>
-        <div className="delete-account-btn" onClick={handleDelete}>Supprimer</div>
-    </form> 
-    )
+      <div className="delete-account-btn" onClick={handleDelete}>Supprimer</div>
+    </form>
+  );
 }
 
-export default InfoForm;
+InfoForm.propTypes = {
+  user: object.isRequired,
+  setUser: func.isRequired,
+  setIsEditOpen: func.isRequired,
+  setIsLogged: func.isRequired,
+};
 
+export default InfoForm;
