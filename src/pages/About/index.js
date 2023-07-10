7786b6ef503181660b3abda94c73d6fa5583f49b
Fred Meziere <Fredzem@outlook.fr>
2023-07-10 14:18:48 +0200

    Refacto: Modification de la page About + css

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -9,18 +9,19 @@ import fred from '../../assets/img/fred.JPG';
 import candice from '../../assets/img/candice.jpeg';
 import marie from '../../assets/img/marie.JPG';
 import ludo from '../../assets/img/ludo.jpg';
+import andy from '../../assets/img/andy.jpg';
 
 function About() {
   return (
-    <div className="About">
-      <h2>L'équipe O'Stretch</h2>
+    <div className="about">
+      <h2 className="about-h2">L'équipe O'Stretch</h2>
       <div className="about-container">
-        <div className="about-card">
-          <img className="about-photo" src={marie} alt="" />
+        <div className="about-container-card">
+          <img className="about-container-card-photo" src={marie} alt="" />
           <div>
-            <p className="about-name">Marie Geneste</p>
-            <p>Product Owner et Développeuse Backend</p>
-            <div className="about-icons">
+            <p className="about-container-card-name">Marie Geneste</p>
+            <p className="about-container-card-p">Product Owner et Développeuse Backend</p>
+            <div className="about-container-card-icons">
               <Link to="https://www.linkedin.com/in/marie-geneste-b254a4251/">
                 <FaLinkedin />
               </Link>
@@ -30,12 +31,12 @@ function About() {
             </div>
           </div>
         </div>
-        <div className="about-card">
-          <img className="about-photo" src={fred} alt="" />
+        <div className="about-container-card">
+          <img className="about-container-card-photo" src={fred} alt="" />
           <div>
-            <p className="about-name">Fred Meziere</p>
-            <p>Git Master et Développeur Backend</p>
-            <div className="about-icons">
+            <p className="about-container-card-name">Fred Meziere</p>
+            <p className="about-container-card-p">Git Master et Développeur Backend</p>
+            <div className="about-container-card-icons">
               <Link to="https://www.linkedin.com/in/fred-meziere/">
                 <FaLinkedin />
               </Link>
@@ -45,12 +46,12 @@ function About() {
             </div>
           </div>
         </div>
-        <div className="about-card">
-          <img className="about-photo" src={candice} alt="" />
+        <div className="about-container-card">
+          <img className="about-container-card-photo" src={candice} alt="" />
           <div>
-            <p className="about-name">Candice Boutet</p>
-            <p>Scrum Master et Développeuse Frontend</p>
-            <div className="about-icons">
+            <p className="about-container-card-name">Candice Boutet</p>
+            <p className="about-container-card-p">Scrum Master et Développeuse Frontend</p>
+            <div className="about-container-card-icons">
               <Link to="https://www.linkedin.com/in/candice-boutet/">
                 <FaLinkedin />
               </Link>
@@ -60,12 +61,12 @@ function About() {
             </div>
           </div>
         </div>
-        <div className="about-card">
-          <img className="about-photo" src="" alt="" />
+        <div className="about-container-card">
+          <img className="about-container-card-photo" src={andy} alt="" />
           <div>
-            <p className="about-name">Andy Vasseur</p>
-            <p>Référent technique et Développeur Frontend</p>
-            <div className="about-icons">
+            <p className="about-container-card-name">Andy Vasseur</p>
+            <p className="about-container-card-p">Référent technique et Développeur Frontend</p>
+            <div className="about-container-card-icons">
               <Link to="https://www.linkedin.com/in/andy-vasseur/">
                 <FaLinkedin />
               </Link>
@@ -75,12 +76,12 @@ function About() {
             </div>
           </div>
         </div>
-        <div className="about-card">
-          <img className="about-photo" src={ludo} alt="" />
+        <div className="about-container-card">
+          <img className="about-container-card-photo" src={ludo} alt="" />
           <div>
-            <p className="about-name">Ludovic Torval</p>
-            <p>Lead Dev et Développeur Frontend</p>
-            <div className="about-icons">
+            <p className="about-container-card-name">Ludovic Torval</p>
+            <p className="about-container-card-p">Développeur Frontend et Backend</p>
+            <div className="about-container-card-icons">
               <Link to="https://www.linkedin.com/in/ludovict/">
                 <FaLinkedin />
               </Link>
