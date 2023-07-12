544327fbfc0022898a9d9cf3acbce00f2a96e96f
Fred Meziere <Fredzem@outlook.fr>
2023-07-12 14:21:12 +0200

    Feat: Forum modifications

--- src/pages/Forum/index.js
+++ src/pages/Forum/index.js
@@ -1,30 +1,27 @@
-import { useState } from 'react';
 import axios from 'axios';
-import { toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { useNavigate } from 'react-router-dom';
+import { Component } from 'react';
+import { NavLink } from 'react-router-dom';
 import Wrapper from '../../components/Wrapper';
 
 // Styles
 import './styles.scss';
 
-function Forum() {
-  return (
-    <div className="forum">
-      <div className="forum-container">
-        <Wrapper
-          wrapperTitle="Bienvenue sur le forum"
-          wrapperDescription="Vous pourrez retrouver ici de nombreux sujets de discussions passionnants"
-        />
-        <div className="forum-container-info">
-          <h2 className="forum-container-p">Les discussions récentes</h2>
-        </div>
-        <div className="forum-container-info">
-          <h2 className="forum-container-p">Les différents sujets</h2>
+
+    return (
+      <div className="forum">
+        <div className="forum-container">
+          <Wrapper
+            wrapperTitle="Bienvenue sur le forum"
+            wrapperDescription="Vous pourrez retrouver ici de nombreux sujets de discussions passionnants"
+          />
+          <div className="forum-container-info">
+            <h2 className="forum-container-p">Les discussions récentes</h2>
+          </div>
+          <div className="forum-container-info">
+            <h2 className="forum-container-p">Les différents sujets</h2>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Forum;
