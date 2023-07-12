609012035e61f92eb0fc3cea685ae313f51758f6
Fred Meziere <Fredzem@outlook.fr>
2023-07-12 15:24:22 +0200

    Feat: Construction du corps du forum

--- src/pages/Forum/index.js
+++ src/pages/Forum/index.js
@@ -1,27 +1,29 @@
+import { useState } from 'react';
 import axios from 'axios';
-import { Component } from 'react';
-import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Wrapper from '../../components/Wrapper';
 
 // Styles
 import './styles.scss';
 
-
-    return (
-      <div className="forum">
-        <div className="forum-container">
-          <Wrapper
-            wrapperTitle="Bienvenue sur le forum"
-            wrapperDescription="Vous pourrez retrouver ici de nombreux sujets de discussions passionnants"
-          />
-          <div className="forum-container-info">
-            <h2 className="forum-container-p">Les discussions récentes</h2>
-          </div>
-          <div className="forum-container-info">
-            <h2 className="forum-container-p">Les différents sujets</h2>
-          </div>
+function Forum() {
+  return (
+    <div className="forum">
+      <Wrapper
+        wrapperTitle="Bienvenue sur le forum"
+        wrapperDescription="Vous pourrez retrouver ici de nombreux sujets de discussions passionnants"
+      />
+      <div className="forum-container">
+        <div className="forum-container-subject">
+          <h2 className="forum-container-subject-h2">Les différentes catégories de sujets</h2>
         </div>
+        <div className="forum-container-discussions">
+          <h2 className="forum-container-discussions-h2">Les discussions récentes</h2>
+        </div>
+
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Forum;
